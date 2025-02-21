b892bd0ea5a907fb7dc30ea99890c467d2b27d37
Lukáš Hejl <hejllukas@gmail.com>
2025-02-21 10:00:00 +0200

    SPE-2698: Fix crash during regions expansion.
    Handles very rare case when both endpoints intersect boundary ExPolygons
    in existing points, so the ZFillFunction callback hasn't been called.
    
    Source: PrusaSlicer commit a3f75
    Issue: https://github.com/prusa3d/PrusaSlicer/issues/12469

--- src/libslic3r/Algorithm/RegionExpansion.cpp
+++ src/libslic3r/Algorithm/RegionExpansion.cpp
@@ -272,6 +272,17 @@ std::vector<WaveSeed> wave_seeds(
             //(front.z() < 0 && back.z() < 0));
             // Hope that at least one end of an open polyline is clipped by the boundary, thus an intersection point is created.
             (front.z() < 0 || back.z() < 0));
+
+        if (front != back && front.z() >= 0 && back.z() >= 0) {
+            // Very rare case when both endpoints intersect boundary ExPolygons in existing points.
+            // So the ZFillFunction callback hasn't been called.
+            continue;
+        } else if (front == back && (front.z() < idx_boundary_end)) {
+            // This should be a very rare exception.
+            // See https://github.com/prusa3d/PrusaSlicer/issues/12469.
+            continue;
+        }
+
         const Intersection *intersection = nullptr;
         auto intersection_point_valid = [idx_boundary_end, idx_src_end](const Intersection &is) {
             return is.first >= 1 && is.first < idx_boundary_end &&
